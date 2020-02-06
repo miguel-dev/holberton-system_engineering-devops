4c5834ec684ebd375d549102f7d7d93b0e04624b
miguel-dev <miguelacortes@zoho.com>
2020-02-06 23:08:51 +0000

    [Feature] 0. Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Scripts using this REST API: https://jsonplaceholder.typicode.com.
+Accepts an employeeID returns information about his/her TODO list progress"""
+
+import requests
+import json
+import sys
+list_users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+list_tasks = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+for user in list_users:
+    if (user.get('id') == int(sys.argv[1])):
+        print("Employee {} is done with".format(user.get('name')), end="")
+tasks = []
+total = 0
+done = 0
+for task in list_tasks:
+    if (task.get('userId') == int(sys.argv[1])):
+        if task.get('completed') is True:
+            tasks.append(task.get('title'))
+            done += 1
+        total += 1
+print(" tasks({:d}/{:d}):".format(done, total))
+for task in tasks:
+    print("\t{}".format(task))
