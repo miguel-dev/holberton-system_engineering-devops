6d46255d9d8aa64c930cac4746bb7a45fbb5ff05
miguel-dev <miguelacortes@zoho.com>
2020-03-21 18:00:47 -0500

    Delete 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-#Creates a file in /tmp
-file { 'creates a file in /tmp':
-  path    => '/tmp/holberton',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
-}
