60ac235f8c902b590ac0f1c8d50746fc39cd5d5f
miguel-dev <miguelacortes@zoho.com>
2020-03-24 17:00:52 +0000

    added commas at end of states

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Creates a file
 file { 'holberton':
-  path    =>  '/tmp/holberton'
-  mode    =>  '0744'
-  owner   =>  'www-data'
-  group   =>  'www-data'
-  content =>  'I love Puppet'
+  path    =>  '/tmp/holberton',
+  mode    =>  '0744',
+  owner   =>  'www-data',
+  group   =>  'www-data',
+  content =>  'I love Puppet',
 }
