3cbd9587611a87e7ce1a1d6f26c1af839cd83e7f
miguel-dev <miguelacortes@zoho.com>
2020-05-05 05:26:36 +0000

    [Feature] 1. Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Accepts employee ID and records all tasks in CSV format"""
+import csv
+import requests
+import sys
+if __name__ == "__main__":
+    url_base = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get('{}users/{}'.format(url_base, sys.argv[1]))
+    dict_user = user.json()
+
+    tasks = requests.get('{}todos?userId={}'.format(url_base, sys.argv[1]))
+    list_tasks = tasks.json()
+
+    with open('{}.csv'.format(sys.argv[1]), mode='w') as csv_file:
+        fieldnames = ["USER_ID",
+                      "USERNAME",
+                      "TASK_COMPLETED_STATUS",
+                      "TASK_TITLE"]
+        writer = csv.DictWriter(csv_file,
+                                fieldnames=fieldnames,
+                                quoting=csv.QUOTE_ALL)
+
+        for task in list_tasks:
+            writer.writerow({"USER_ID": sys.argv[1],
+                             "USERNAME": dict_user.get("username"),
+                             "TASK_COMPLETED_STATUS": task.get("completed"),
+                             "TASK_TITLE": task.get("title")})
