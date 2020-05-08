660b342ca7e97fa01c41e9a34ff1fec9129eefd6
miguel-dev <miguelacortes@zoho.com>
2020-05-08 19:49:29 +0000

    [Fixes] Doesn't allow redirections and queries https://api.reddit.com/..

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns number of subscribers for given subreddit"""
-    url = 'http://reddit.com/r/{}/about.json'.format(subreddit)
+    url = 'https://api.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'user-agent': 'Miguel'}
-    r = requests.get(url, headers=headers)
-    if not (r.status_code == requests.codes.ok):
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if (r.status_code != requests.codes.ok):
         return 0
     else:
         reddit_info = r.json()
