e8a523c664b28e37d9b3b7daeece06d955a98d74
miguel-dev <miguelacortes@zoho.com>
2020-05-08 04:47:33 +0000

    [Feature] 1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Queries Reddit API and prints the titles of the first 10 hot posts
+a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """Prints titles of first 10 hot posts of a given subreddit"""
+    url = 'https://reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'user-agent': 'linux:api_advanced:v1.2 (by /u/miguel-dev)'}
+    r = requests.get(url, headers=headers)
+    if not (r.status_code == requests.codes.ok):
+        return 0
+    else:
+        dict_hot = r.json()
+        hot_posts = dict_hot.get("data").get('children')
+        for post in hot_posts:
+            print(post.get('data').get('title'))
