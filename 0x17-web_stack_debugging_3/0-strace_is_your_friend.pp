3c9c67983763e9769a2c5da0cfff4efe78179097
miguel-dev <miguelacortes@zoho.com>
2020-05-20 02:18:12 +0000

    [Fixes] Adds .php at the end of file path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 #Replaces phpp for php
 exec { 'replace':
-    command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings;',
+    command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php;',
 }
