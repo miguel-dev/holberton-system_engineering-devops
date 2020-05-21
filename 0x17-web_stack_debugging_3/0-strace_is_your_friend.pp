39d75df097d99b33bb5a49adb2fbc6d740ee6b0a
miguel-dev <miguelacortes@zoho.com>
2020-05-21 04:00:59 +0000

    [Fixes] Command first and then path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #Replaces phpp for php                                                                
 exec { 'replace':
-    path    => ['/bin', '/usr/bin'],
     command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php; sudo service apac
+    path    => ['/bin', '/usr/bin'],
 he2 restart;',
 }
