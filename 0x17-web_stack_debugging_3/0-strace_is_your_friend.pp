d4ee79970d112842f8e67421bc4dda0256792d59
miguel-dev <miguelacortes@zoho.com>
2020-05-21 03:29:52 +0000

    [Fixes] Restarts apache2

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 #Replaces phpp for php
 exec { 'replace':
-    command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php;',
+    command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php; sudo service apache2 restart;',
 }
