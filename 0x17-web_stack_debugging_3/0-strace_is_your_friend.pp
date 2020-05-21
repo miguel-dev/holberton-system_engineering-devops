5331bb37de799717e9f31f19644788eab071e385
miguel-dev <miguelacortes@zoho.com>
2020-05-21 04:03:30 +0000

    [Fixes] Command line was broken since I did copy and paste from container

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 #Replaces phpp for php                                                                
 exec { 'replace':
-    command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php; sudo service apac
+    command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php; sudo service apache2 restart;',
     path    => ['/bin', '/usr/bin'],
-he2 restart;',
 }
